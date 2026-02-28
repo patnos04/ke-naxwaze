a8b89e1941aa5c2fc7b434e0a693555e8a22b789
patnos04 <seyfettinesin@gmail.com>
2026-02-28 09:08:31 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    minify: 'esbuild',
+    commonjsOptions: { transformMixedEsModules: true }
+  }
 })
